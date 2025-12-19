2b48155b3672979abc0a5ed1ff7bbb0e279a32d2
Ketly <ketlydeoliveira3@gmail.com>
2025-12-19 20:55:16 -0300

    Aula01.py
    Aprendizado sobre f-string

---
+++ Aula01.py
@@ -0,0 +1,5 @@
+# Definindo uma variável do tipo texto (string)
+nome = ("Seu nome")
+
+#Usando uma f-string para exibir uma mensagem personalizada na tela
+print = (f" E aí {nome}! Seu primeiro código está salvo")
