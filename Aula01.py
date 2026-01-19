936cf7f755c22c594790cb383fd4978d778f9767
Ketly <ketlydeoliveira3@gmail.com>
2026-01-19 10:43:34 -0300

    Update and rename Aula01.py to aula.py

--- Aula01.py
+++
@@ -1,5 +0,0 @@
-# Definindo uma variável do tipo texto (string)
-nome = ("Seu nome")
-
-#Usando uma f-string para exibir uma mensagem personalizada na tela
-print = (f" E aí {nome}! Seu primeiro código está salvo")
---
+++ aula.py
@@ -0,0 +1,22 @@
+# Definindo uma variável do tipo texto (string)
+nome = ("Seu nome")
+
+#Usando uma f-string para exibir uma mensagem personalizada na tela
+print = (f" E aí {nome}! Seu primeiro código está salvo")
+
+#Condicional: O código faz uma pergunta e toma uma decisão baseada na resposta.
+
+idade = int(input("Digite sua idade aqui:"))
+if idade >= 18:    
+    print("Acesso liberado")
+else:
+    print ("Acesso negado")    
+
+#Tipos primitivos: São os tipos básicos de dado que o Python entende:
+
+#tipo.        #descrição.                #exemplo
+
+int.          número inteiro.            idade = 20
+float.        número real (decimal).     altura = 1.75           
+str.          texto (string).            nome = "Ana"
+bool.         verdadeiro/falso.          ativo = "true"
