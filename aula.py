6817cc4f697ffbe7f61e8192ac728394ce33edb7
Ketly <ketlydeoliveira3@gmail.com>
2026-02-05 20:08:26 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -45,3 +45,24 @@ if compra <= limite:
     print ("Compra aprovada!")
 else:
      print ("Compra recusada!")
+
+
+# PROJETO: Validador de Rotina com Lógica Booleana
+
+# Pergunta se treinou e guarda a resposta
+treinou_hoje = input("Voce treinou hoje? (sim/nao): ")
+
+# Pergunta se comeu o pão e guarda a resposta
+comeu_pao = input("Comeu seu pao frances? (sim/nao): ")
+
+# Se treinou E comeu o pão, faz o que está abaixo
+if treinou_hoje == "sim" and comeu_pao == "sim":
+    
+    # Mensagem caso as duas respostas sejam "sim"
+    print("Status: Dia de vitoria! Meta batida. ✅")
+
+# Caso contrário (se uma das respostas for "nao")
+else:
+    
+    # Mensagem caso algo tenha faltado
+    print("Status: Ops, faltou alguma coisa hoje. ❌")
